30b427786abec956e506ea380666b7c3ede3988b
Beto Figueiredo <herberto_f@yahoo.com.br>
2019-11-16 18:04:21 -0300

    [master] tests: Function findLastBlock basic tests

--- src/functions/findLastColumnFromPreviousBlock/findLastColumnFromPreviousBlock.js
+++ src/functions/findLastColumnFromPreviousBlock/findLastColumnFromPreviousBlock.js
@@ -1,7 +1,10 @@
 export const findLastColumnFromPreviousBlock = (columns, part_id, block_id) => {
-	const { by_id } = columns;
+	const by_id = (columns || {}).by_id || {};
 	for (let i = 1; i <= 1000; i++) {
-		if (by_id[`${part_id}-${block_id}-${i}`] === undefined) {
+		if (
+			by_id[`${part_id}-${block_id}-${i}`] === undefined
+			&& i > 1
+		) {
 			return i - 1;
 		}
 	}
--- src/functions/findLastColumnFromPreviousBlock/findLastColumnFromPreviousBlock.test.js
+++ src/functions/findLastColumnFromPreviousBlock/findLastColumnFromPreviousBlock.test.js
@@ -1,7 +1,36 @@
 import { findLastColumnFromPreviousBlock } from './findLastColumnFromPreviousBlock';
 
-test('findLastColumnFromPreviousBlock', () => {
-	// expect(caracteristicaType([], 0, 'texto')).toBe('texto');
-	// expect(caracteristicaType([], null, 'texto')).toBe('texto');
+test('should not crash', () => {
+	expect(findLastColumnFromPreviousBlock()).toBe(1);
+	expect(findLastColumnFromPreviousBlock(null, null, null)).toBe(1);
+});
+
+test('should find column 4', () => {
+	const columns = {
+		by_id: {
+			'1-1-1': {
+				part_id: 1,
+				block_id: 1,
+				id: '1-1-1',
+			},
+			'1-1-2': {
+				part_id: 1,
+				block_id: 1,
+				id: '1-1-2',
+			},
+			'1-1-3': {
+				part_id: 1,
+				block_id: 1,
+				id: '1-1-3',
+			},
+			'1-1-4': {
+				part_id: 1,
+				block_id: 1,
+				id: '1-1-4',
+			},
+		},
+	};
+	const last_column = findLastColumnFromPreviousBlock(columns, 1, 1);
+	expect(last_column).toBe(4);
 });
 

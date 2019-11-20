74c94d13a6e17273c70b7ad26361df00f43afc09
Beto Figueiredo <herberto_f@yahoo.com.br>
2019-11-20 19:56:44 -0300

    [master] refactor: Remove unused file

--- src/test/react-redux-hooks.js
+++
@@ -1,5 +0,0 @@
-// to mock it in test
-import { useSelector as originalUseSelector, useDispatch as originalUseDispatch } from 'react-redux';
-
-export const useSelector = (state) => originalUseSelector(state);
-export const useDispatch = () => originalUseDispatch();

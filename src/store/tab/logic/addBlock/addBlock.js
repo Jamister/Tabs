b80ec12e1d8e3a928b636add6dcc59f21cf99366
Beto Figueiredo <herberto_f@yahoo.com.br>
2019-11-20 21:09:21 -0300

    [master] feat: Add 5 columns when adding a new block, not 4

--- src/store/tab/logic/addBlock/addBlock.js
+++ src/store/tab/logic/addBlock/addBlock.js
@@ -22,14 +22,14 @@ export const addBlock = (state, action) => {
 			},
 		},
 	};
-	const columns_all_ids = [1, 2, 3, 4].map(column_id => (
+	const columns_all_ids = [1, 2, 3, 4, 5].map(column_id => (
 		`${next_block_id}-${column_id}`
 	));
 	const all_ids = [
 		...state.columns.all_ids,
 		...columns_all_ids,
 	].filter((el, i, a) => i === a.indexOf(el));
-	const columns_by_id = [1, 2, 3, 4].reduce((result, column_id) => ({
+	const columns_by_id = [1, 2, 3, 4, 5].reduce((result, column_id) => ({
 		...result,
 		[`${next_block_id}-${column_id}`]: {
 			part_id,

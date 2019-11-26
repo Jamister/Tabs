b5755407d0033cae0d010eb3be01ee1c6c8a1496
Beto Figueiredo <herberto_f@yahoo.com.br>
2019-11-25 21:33:18 -0300

    [master] tests: Basic tests for the addBlock redux logic

--- src/store/tab/logic/addBlock/addBlock.js
+++ src/store/tab/logic/addBlock/addBlock.js
@@ -1,32 +1,18 @@
-export const addBlock = (state, action) => {
-	const { part_id } = action;
-	let next_block_id = `${part_id}-${1}`;
-	let block_id = 0;
-	for (let i = 1; i <= 1000; i++) {
-		if (state.blocks.by_id[`${part_id}-${i}`] === undefined) {
-			next_block_id = `${part_id}-${i}`;
-			block_id = i;
-			break;
-		}
-	}
-	const blocks = {
-		all_ids: [
-			...state.blocks.all_ids,
-			next_block_id,
-		],
-		by_id: {
-			...state.blocks.by_id,
-			[next_block_id]: {
-				part_id,
-				id: next_block_id,
-			},
-		},
-	};
+function createColumns(
+	state,
+	block_id,
+	next_block_id,
+	part_id,
+) {
+	const columns = state.columns || {};
+	const all_ids = columns.all_ids || [];
+	const by_id = columns.by_id || {};
+
 	const columns_all_ids = [1, 2, 3, 4, 5].map(column_id => (
 		`${next_block_id}-${column_id}`
 	));
-	const all_ids = [
-		...state.columns.all_ids,
+	const _all_ids = [
+		...all_ids,
 		...columns_all_ids,
 	].filter((el, i, a) => i === a.indexOf(el));
 	const columns_by_id = [1, 2, 3, 4, 5].reduce((result, column_id) => ({
@@ -37,16 +23,62 @@ export const addBlock = (state, action) => {
 			id: `${next_block_id}-${column_id}`,
 		},
 	}), {});
-	const columns = {
-		all_ids,
+	return {
+		all_ids: _all_ids,
 		by_id: {
-			...state.columns.by_id,
+			...by_id,
 			...columns_by_id,
 		},
 	};
+}
+
+export const addBlock = (_state, _action) => {
+	const state = _state || {};
+	const action = _action || {};
+	const blocks = state.blocks || {};
+	const all_ids = blocks.all_ids || [];
+	const by_id = blocks.by_id || {};
+	const { part_id } = action;
+	let next_block_id = `${part_id}-${1}`;
+	let block_id = 0;
+
+	if (part_id === undefined) {
+		return { ...state };
+	}
+
+	for (let i = 1; i <= 1000; i++) {
+		if (by_id[`${part_id}-${i}`] === undefined) {
+			next_block_id = `${part_id}-${i}`;
+			block_id = i;
+			break;
+		}
+	}
+
+	const _all_ids = [
+		...all_ids,
+		next_block_id,
+	].filter((el, i, a) => i === a.indexOf(el));
+	const _blocks = {
+		all_ids: _all_ids,
+		by_id: {
+			...by_id,
+			[next_block_id]: {
+				part_id,
+				id: next_block_id,
+			},
+		},
+	};
+
+	const columns = createColumns(
+		state,
+		block_id,
+		next_block_id,
+		part_id,
+	);
+
 	return {
 		...state,
-		blocks,
+		blocks: _blocks,
 		columns,
 	};
 };

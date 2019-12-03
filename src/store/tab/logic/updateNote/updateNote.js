878d5d3966119d15d056436bc3ae5f237fa46e0f
Beto Figueiredo <herberto_f@yahoo.com.br>
2019-12-03 00:09:04 -0300

    [master] refactor: Function to delete columns, not working yet

--- src/store/tab/logic/updateNote/updateNote.js
+++ src/store/tab/logic/updateNote/updateNote.js
@@ -1,14 +1,14 @@
 // Functions
 import { returnKeyValue } from '../../../../functions/returnKeyValue';
 
-function addColumnIfLastOne(state) {
-	const columns = state.columns || {};
+function addColumnIfLastOne(state, columns) {
 	const all_ids = columns.all_ids || [];
 	const by_id = columns.by_id || {};
 	const selected_note = state.selected_note || {};
 	const { p, b, c } = selected_note;
 	const next_column_id = `${p}-${b}-${c + 1}`;
 	const last_column = by_id[next_column_id] === undefined;
+
 	if (last_column) {
 		const new_columns = {
 			all_ids: [
@@ -30,7 +30,40 @@ function addColumnIfLastOne(state) {
 	return { ...columns };
 }
 
-// function removeColumnsIfEmpty(state) {
+// function deleteColumnIfEmpty(state, columns, value, key_code) {
+// 	if (value !== '') {
+// 		return { ...columns };
+// 	}
+
+// 	const all_ids = columns.all_ids || [];
+// 	const by_id = columns.by_id || {};
+// 	const selected_note = state.selected_note || {};
+// 	const { p, b, c } = selected_note;
+// 	const lines = state.lines || [];
+
+// 	const notes_on_column = lines.reduce((res, cur) => {
+// 		const note_id = `${p}-${b}-${c}-${cur}`;
+// 		const note_value = ((state.notes || {})[note_id] || {}).value || '';
+// 		if (note_value !== '') {
+// 			return note_value;
+// 		}
+
+// 		return res;
+// 	}, '');
+
+// 	if (notes_on_column === '' && key_code === 46) {
+// 		const column_id = `${p}-${b}-${c + 1}`;
+// 		const _all_ids = all_ids.filter(x => x !== column_id);
+// 		const _by_id = { ...by_id };
+// 		delete _by_id[column_id];
+// 		const new_columns = {
+// 			all_ids: _all_ids,
+// 			by_id: _by_id,
+// 		};
+// 		return new_columns;
+// 	}
+
+// 	return { ...columns };
 // }
 
 export const updateNote = (_state, _action) => {
@@ -56,9 +89,15 @@ export const updateNote = (_state, _action) => {
 		notes,
 	};
 
+	const columns = state.columns || {};
+
 	// add column in case its the last one
-	const columns = addColumnIfLastOne(state);
-	data_to_update.columns = columns;
+	const _columns = addColumnIfLastOne(state, columns);
+
+	// delete column if key is delete and its empty
+	// const __columns = deleteColumnIfEmpty(state, _columns, value, action.key_code);
+
+	data_to_update.columns = _columns;
 
 	// TODO remove empty column at the end
 

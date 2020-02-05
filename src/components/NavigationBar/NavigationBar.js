36755061ccaa84a566e3f9f8961a580c0552f423
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-02-05 19:24:00 -0300

    [issue/4] feat: Remove logic from navigationbar to its own component

--- src/components/NavigationBar/NavigationBar.js
+++ src/components/NavigationBar/NavigationBar.js
@@ -1,35 +1,22 @@
 import React from 'react';
-import { useDispatch } from 'react-redux';
 
 // CSS
 import CSSModules from 'react-css-modules';
 import styles from './style.module.sass';
 
-// Actions
-import { exportTabFormatTxt } from '../../store/tab/actions';
+// Components
+import ExportTab from '../ExportTab';
 
-function NavigationBar() {
-	const dispatch = useDispatch();
-
-	function exportTab() {
-		dispatch(
-			exportTabFormatTxt(),
-		);
-	}
-
-	return (
-		<div className="grid-container full">
-			<div className="grid-x">
-				<div className="medium-12 cell">
-					<div styleName="top-bar">
-						<button type="button" onClick={exportTab}>
-							Export
-						</button>
-					</div>
+const NavigationBar = () => (
+	<div className="grid-container full">
+		<div className="grid-x">
+			<div className="medium-12 cell">
+				<div styleName="top-bar">
+					<ExportTab />
 				</div>
 			</div>
 		</div>
-	);
-}
+	</div>
+);
 
 export default CSSModules(NavigationBar, styles, { allowMultiple: true });

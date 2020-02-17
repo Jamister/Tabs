d56eb8c924ec72befd83883895b6c3e0e82c2a71
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-02-17 18:40:48 -0300

    [master] feat: New functions to handle instruments and pressed keys

--- src/store/tab/reducers.js
+++ src/store/tab/reducers.js
@@ -2,8 +2,10 @@ import tab from './store';
 
 // Actions external logic
 import { addBlock } from './logic/addBlock';
+import { clearPressedKey } from './logic/clearPressedKey';
 import { clearSelectNote } from './logic/clearSelectNote';
 import { exportTabFormatTxt } from './logic/exportTabFormatTxt';
+import { holdPressedKey } from './logic/holdPressedKey';
 import { moveSelectedNote } from './logic/moveSelectedNote';
 import { removeBlock } from './logic/removeBlock';
 import { selectNote } from './logic/selectNote';
@@ -11,8 +13,10 @@ import { updateNote } from './logic/updateNote';
 
 const funcs = {
 	ADD_BLOCK: addBlock,
+	CLEAR_PRESSED_KEY: clearPressedKey,
 	CLEAR_SELECT_NOTE: clearSelectNote,
 	EXPORT_TAB_TXT: exportTabFormatTxt,
+	HOLD_PRESSED_KEY: holdPressedKey,
 	MOVE_SELECTED_NOTE: moveSelectedNote,
 	REMOVE_BLOCK: removeBlock,
 	SELECT_NOTE: selectNote,

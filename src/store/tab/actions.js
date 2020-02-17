ae2d28d67c371d3fa078feedbdff580c400c4cb1
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-02-17 18:37:48 -0300

    [master] fix: Only change the order of functions

--- src/store/tab/actions.js
+++ src/store/tab/actions.js
@@ -1,17 +1,21 @@
 import {
-	SELECT_NOTE,
-	CLEAR_SELECT_NOTE,
-	UPDATE_NOTE,
-	MOVE_SELECTED_NOTE,
 	ADD_BLOCK,
-	REMOVE_BLOCK,
+	CLEAR_PRESSED_KEY,
+	CLEAR_SELECT_NOTE,
 	EXPORT_TAB_TXT,
+	HOLD_PRESSED_KEY,
+	MOVE_SELECTED_NOTE,
+	REMOVE_BLOCK,
+	SELECT_NOTE,
+	UPDATE_NOTE,
 } from './types';
 
-export const selectNote = (p, b, c, l) => ({ type: SELECT_NOTE, p, b, c, l });
-export const clearSelectNote = () => ({ type: CLEAR_SELECT_NOTE });
-export const updateNote = (key_code) => ({ type: UPDATE_NOTE, key_code });
-export const moveSelectedNote = (key_code) => ({ type: MOVE_SELECTED_NOTE, key_code });
 export const addBlock = (part_id) => ({ type: ADD_BLOCK, part_id });
-export const removeBlock = (part_id) => ({ type: REMOVE_BLOCK, part_id });
+export const clearPressedKey = () => ({ type: CLEAR_PRESSED_KEY });
+export const clearSelectNote = () => ({ type: CLEAR_SELECT_NOTE });
 export const exportTabFormatTxt = () => ({ type: EXPORT_TAB_TXT });
+export const holdPressedKey = (key_code) => ({ type: HOLD_PRESSED_KEY, key_code });
+export const moveSelectedNote = (key_code) => ({ type: MOVE_SELECTED_NOTE, key_code });
+export const removeBlock = (part_id) => ({ type: REMOVE_BLOCK, part_id });
+export const selectNote = (p, b, c, l) => ({ type: SELECT_NOTE, p, b, c, l });
+export const updateNote = (key_code) => ({ type: UPDATE_NOTE, key_code });

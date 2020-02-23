bac1efac5135bb1225938af05a5470c53fb1978f
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-02-23 18:12:20 -0300

    [master] refactor: Better code organization on the addPart function

--- src/store/tab/logic/addPart/addPart.js
+++ src/store/tab/logic/addPart/addPart.js
@@ -5,82 +5,116 @@ export const addPart = (state = {}) => {
 	const next_state = { ...state };
 	let next_part_id = 1;
 
+	function updateNextState({ object, field, value }) {
+		next_state[object] = {
+			...next_state[object],
+			[field]: value,
+		};
+	}
+
 	function returnUpdatedStore() {
 		return { ...next_state };
 	}
 
-	function addColumnsToStore(block_full_id) {
-		const {
-			new_columns_all_ids,
-			new_columns_by_id,
-		} = createColumns(block_full_id);
-
-		const columns_all_ids = (next_state.columns || {}).all_ids || [];
-		const columns_all_ids_updated = [
-			...columns_all_ids,
-			...new_columns_all_ids,
-		].filter((el, i, a) => i === a.indexOf(el));
-
+	function addColumnsToIdsObject(block_full_id) {
+		const { new_columns_by_id } = createColumns(block_full_id);
 		const columns_by_id = (next_state.columns || {}).by_id || {};
 		const columns_by_id_updated = {
 			...columns_by_id,
 			...new_columns_by_id,
 		};
-
-		next_state.columns = {
-			all_ids: columns_all_ids_updated,
-			by_id: columns_by_id_updated,
-		};
+		updateNextState({
+			object: 'columns',
+			field: 'by_id',
+			value: columns_by_id_updated,
+		});
 
 		return returnUpdatedStore();
 	}
 
-	function addBlockToStore() {
-		const block_full_id = `${next_part_id}-1`;
-		const block_object = {
-			part_id: next_part_id,
-			id: block_full_id,
-		};
-		const blocks_all_ids = (next_state.blocks || {}).all_ids || [];
-		const blocks_all_ids_updated = [
-			...blocks_all_ids,
-			block_full_id,
-		];
+	function addColumnsToIdsList(block_full_id) {
+		const { new_columns_all_ids } = createColumns(block_full_id);
+		const columns_all_ids = (next_state.columns || {}).all_ids || [];
+		const columns_all_ids_updated = [
+			...columns_all_ids,
+			...new_columns_all_ids,
+		].filter((el, i, a) => i === a.indexOf(el));
+		updateNextState({
+			object: 'columns',
+			field: 'all_ids',
+			value: columns_all_ids_updated,
+		});
+
+		return addColumnsToIdsObject(block_full_id);
+	}
+
+	function addBlockToIdsObject(block_object) {
 		const blocks_by_id = (next_state.blocks || {}).by_id || {};
 		const blocks_by_id_updated = {
 			...blocks_by_id,
-			[block_full_id]: block_object,
+			[block_object.id]: block_object,
 		};
-		next_state.blocks = {
-			all_ids: blocks_all_ids_updated,
-			by_id: blocks_by_id_updated,
-		};
+		updateNextState({
+			object: 'blocks',
+			field: 'by_id',
+			value: blocks_by_id_updated,
+		});
 
-		return addColumnsToStore(block_full_id);
+		return addColumnsToIdsList(block_object.id);
 	}
 
-	function addPartToStore() {
-		const all_ids = (next_state.parts || {}).all_ids || [];
-		const all_ids_updated = [
-			...all_ids,
-			next_part_id,
+	function addBlockToIdsList(block_object) {
+		const blocks_all_ids = (next_state.blocks || {}).all_ids || [];
+		const blocks_all_ids_updated = [
+			...blocks_all_ids,
+			block_object.id,
 		];
-		const by_id = (next_state.parts || {}).by_id || {};
-		const by_id_updated = {
-			...by_id,
+		updateNextState({
+			object: 'blocks',
+			field: 'all_ids',
+			value: blocks_all_ids_updated,
+		});
+
+		return addBlockToIdsObject(block_object);
+	}
+
+	function createBlockObject() {
+		const block_full_id = `${next_part_id}-1`;
+		const block_object = { id: block_full_id };
+
+		return addBlockToIdsList(block_object);
+	}
+
+	function addPartToIdsObject() {
+		const parts_by_id = (next_state.parts || {}).by_id || {};
+		const parts_by_id_updated = {
+			...parts_by_id,
 			[next_part_id]: {
 				id: next_part_id,
 			},
 		};
-		next_state.parts = {
-			all_ids: all_ids_updated,
-			by_id: by_id_updated,
-		};
+		updateNextState({
+			object: 'parts',
+			field: 'by_id',
+			value: parts_by_id_updated,
+		});
 
-		return addBlockToStore();
+		return createBlockObject();
 	}
 
-	function getLastPartId() {
+	function addPartToIdsList() {
+		const parts_all_ids = (next_state.parts || {}).all_ids || [];
+		const parts_all_ids_updated = [...parts_all_ids, next_part_id];
+		updateNextState({
+			object: 'parts',
+			field: 'all_ids',
+			value: parts_all_ids_updated,
+		});
+
+		return addPartToIdsObject();
+	}
+
+	function getNextPartId() {
 		const all_ids = parts.all_ids || [];
 		const last_part_id = all_ids
 			.reduce((result, current) => (
@@ -88,8 +122,8 @@ export const addPart = (state = {}) => {
 			), 0);
 		next_part_id = last_part_id + 1;
 
-		return addPartToStore();
+		return addPartToIdsList();
 	}
 
-	return getLastPartId();
+	return getNextPartId();
 };

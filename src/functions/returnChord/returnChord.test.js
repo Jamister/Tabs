237df1d2c9688226ec8718c19279abfaad3bc232
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-02-26 19:14:26 -0300

    [master] tests: Fixed test for the returnChord function

--- src/functions/returnChord/returnChord.test.js
+++ src/functions/returnChord/returnChord.test.js
@@ -1,12 +1,22 @@
-import { returnKeyValue } from './returnKeyValue';
+import { returnChord } from './returnChord';
 
-test('should return default', () => {
-	expect(returnKeyValue()).toBe('');
-	expect(returnKeyValue(null, null)).toBe('');
-	expect(returnKeyValue(36, null)).toBe('');
-	expect(returnKeyValue(null, '')).toBe('');
+it('should return default (null)', () => {
+	const blank = returnChord();
+	expect(blank).toBe(null);
+
+	const wrong_values = returnChord({}, {});
+	expect(wrong_values).toBe(null);
+
+	const wrong_values2 = returnChord(null, null);
+	expect(wrong_values2).toBe(null);
 });
 
-test('should return prevalue', () => {
-	expect(returnKeyValue(36, '22')).toBe('22');
+it('should return C chord', () => {
+	const c_chord = returnChord(67, 'guitar');
+	expect(c_chord).toStrictEqual([0, 3, 2, 0, 1, 0]);
+});
+
+it('should return F chord', () => {
+	const f_chord = returnChord(70, 'guitar');
+	expect(f_chord).toStrictEqual([1, 3, 3, 2, 1, 1]);
 });

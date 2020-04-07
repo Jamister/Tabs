6b1e66847038efe07dd4fad63047768b6e2000cb
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-04-06 22:26:12 -0300

    [master] feat: Map all basic chords

--- src/functions/returnChord/returnChord.js
+++ src/functions/returnChord/returnChord.js
@@ -1,19 +1,48 @@
 import instruments from '../../instruments';
 
 export const returnChord = (key_code, instrument) => {
+	let chord_key = '';
+
 	switch (key_code) {
 	// C
 	case 67: {
-		const chord = instruments[instrument].chords.C[1];
-		return chord;
+		chord_key = 'C';
+		break;
+	}
+	// D
+	case 68: {
+		chord_key = 'D';
+		break;
+	}
+	// E
+	case 69: {
+		chord_key = 'E';
+		break;
 	}
 	// F
 	case 70: {
-		const chord = instruments[instrument].chords.F[1];
-		return chord;
+		chord_key = 'F';
+		break;
+	}
+	// G
+	case 71: {
+		chord_key = 'G';
+		break;
+	}
+	// A
+	case 65: {
+		chord_key = 'A';
+		break;
+	}
+	// B
+	case 66: {
+		chord_key = 'B';
+		break;
+	}
+	// default
+	default:
 	}
 
-	// default
-	default: return null;
-	}
+	const chord = (instruments[instrument].chords[chord_key] || {})[1] || null;
+	return chord;
 };

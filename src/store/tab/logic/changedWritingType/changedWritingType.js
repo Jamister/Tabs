1b7c6a515eb4c7a8d5e1987c04367a60d9639cc3
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-04-06 22:27:08 -0300

    [master] fix: Clear selected note when writing type is changed

--- src/store/tab/logic/changedWritingType/changedWritingType.js
+++ src/store/tab/logic/changedWritingType/changedWritingType.js
@@ -1,7 +1,14 @@
 export const changedWritingType = (state = {}, action = {}) => {
-	const writing = action.writing || 'notes';
+	const user_is_writing = action.writing || 'notes';
+	const selected_note = {
+		p: 0,
+		b: 0,
+		c: 0,
+		l: 0,
+	};
 	return {
 		...state,
-		user_is_writing: writing,
+		selected_note,
+		user_is_writing,
 	};
 };

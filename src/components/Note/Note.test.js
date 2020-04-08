d592fdb580ed9bac7e90a4b78ff77e82da2b6e72
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-04-07 21:40:36 -0300

    [master] test: Testing with @testing-library for Note component

--- src/components/Note/Note.test.js
+++ src/components/Note/Note.test.js
@@ -1,52 +1,77 @@
 import React from 'react';
-import Enzyme, { shallow, mount, render } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-import { useSelector, useDispatch } from 'react-redux';
+import { fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import renderWithRedux from '../../tests/setupReduxTests';
+
+// Store
+import tab from '../../store/tab/store';
+
+// Components
 import Note from './Note';
 
-Enzyme.configure({ adapter: new Adapter() });
-
-jest.mock('react-redux', () => ({
-	useDispatch: jest.fn(),
-	useSelector: jest.fn(),
-}));
-
-describe('Note 1', () => {
-	beforeAll(() => {
-		useSelector.mockImplementation(() => (null))
-	});
-
-	it('should not crash Note component', () => {
-		const wrapper = shallow(
-			<Note
-				part_id={1}
-				block_id={1}
-				column_id={1}
-				line_id={1}
-			/>
+describe('Note', () => {
+	it('should not crash component', async () => {
+		const props = {
+			part_id: 1,
+			block_id: 1,
+			column_id: 1,
+			line_id: 1,
+		};
+		const { getByTestId } = renderWithRedux(
+			<Note {...props} />,
+			{ initialState: { tab: tab } },
 		);
-		const html_elem = wrapper.find('[data-test="note-render"]');
-		expect(html_elem).toHaveLength(1);
 	});
+
+	// it('should press number 2', async () => {
+	// 	const { getByTestId } = renderWithRedux(
+	// 		<Router><Tab /></Router>,
+	// 		{ initialState: { tab: tab } },
+	// 	);
+
+	// 	const wrapper = getByTestId('wrapper');
+	// 	fireEvent.keyDown(wrapper, { key: '2', code: '50' });
+	// 	expect(getByTestId('tab-button-active')).toHaveTextContent('Vendas');
+	// });
 });
 
-describe('Note 2', () => {
-	beforeAll(() => {
-		useSelector.mockImplementation(() => ({
-			p: 1, b: 1, c: 1, l: 1,
-		}))
-	});
 
-	it('should render NoteValue', () => {
-		const wrapper = shallow(
-			<Note
-				part_id={1}
-				block_id={1}
-				column_id={1}
-				line_id={1}
-			/>
-		);
-		const html_elem = wrapper.find('NoteValue');
-		expect(html_elem).toHaveLength(1);
-	});
-});
+// describe('Note 1', () => {
+// 	beforeAll(() => {
+// 		useSelector.mockImplementation(() => (null))
+// 	});
+
+// 	it('should not crash Note component', () => {
+// 		const wrapper = shallow(
+// 			<Note
+// 				part_id={1}
+// 				block_id={1}
+// 				column_id={1}
+// 				line_id={1}
+// 			/>
+// 		);
+// 		const html_elem = wrapper.find('[data-test="note-render"]');
+// 		expect(html_elem).toHaveLength(1);
+// 	});
+// });
+
+// describe('Note 2', () => {
+// 	beforeAll(() => {
+// 		useSelector.mockImplementation(() => ({
+// 			p: 1, b: 1, c: 1, l: 1,
+// 		}))
+// 	});
+
+// 	it('should render NoteValue', () => {
+// 		const wrapper = shallow(
+// 			<Note
+// 				part_id={1}
+// 				block_id={1}
+// 				column_id={1}
+// 				line_id={1}
+// 			/>
+// 		);
+// 		const html_elem = wrapper.find('NoteValue');
+// 		expect(html_elem).toHaveLength(1);
+// 	});
+// });

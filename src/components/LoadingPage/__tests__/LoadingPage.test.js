3dbed86ffba56eef4dee2e110e32f7a7298d2c93
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-14 21:12:06 -0300

    [master] refactor: LoadingPage tests moved to it's own folder

--- src/components/LoadingPage/__tests__/LoadingPage.test.js
+++ src/components/LoadingPage/__tests__/LoadingPage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Enzyme, { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import LoadingPage from './LoadingPage';
+import LoadingPage from '../LoadingPage';
 
 Enzyme.configure({ adapter: new Adapter() });
 

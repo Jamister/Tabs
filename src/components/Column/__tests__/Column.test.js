a47f7d2790f92380d0596bbb96c56d6477a94c67
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-05-14 21:10:06 -0300

    [master] refactor: Tests moved to own folder

--- src/components/Column/__tests__/Column.test.js
+++ src/components/Column/__tests__/Column.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Enzyme, { shallow, mount, render } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { useSelector, useDispatch } from 'react-redux';
-import Column from './Column';
+import Column from '../Column';
 
 Enzyme.configure({ adapter: new Adapter() });
 

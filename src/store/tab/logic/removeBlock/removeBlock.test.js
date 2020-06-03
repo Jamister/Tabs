0a1b4e884879b0f6b87fb251fdf65abb96e0b386
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-06-02 22:57:17 -0300

    [master] refactor: removeBlock empty test for now

--- src/store/tab/logic/removeBlock/removeBlock.test.js
+++
@@ -1,7 +0,0 @@
-import { removeBlock } from './removeBlock';
-
-test('removeBlock', () => {
-	// expect(caracteristicaType([], 0, 'texto')).toBe('texto');
-	// expect(caracteristicaType([], null, 'texto')).toBe('texto');
-});
-

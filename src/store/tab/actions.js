965edcf90aa444b11b552a8e3e950b35a7492bf8
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-06-02 22:47:15 -0300

    [master] refactor: Better code style in redux actions file

--- src/store/tab/actions.js
+++ src/store/tab/actions.js
@@ -1,26 +1,58 @@
-import {
-	ADD_BLOCK,
-	ADD_PART,
-	CLEAR_PRESSED_KEY,
-	CLEAR_SELECT_NOTE,
-	EXPORT_TAB_TXT,
-	HOLD_PRESSED_KEY,
-	MOVE_SELECTED_NOTE,
-	REMOVE_BLOCK,
-	SELECT_NOTE,
-	UPDATE_NOTE,
-	CHANGED_WRITING_TYPE,
-} from './types';
+import * as types from './types';
 
-export const addBlock = (part_id) => ({ type: ADD_BLOCK, part_id });
-export const addPart = () => ({ type: ADD_PART });
-export const clearPressedKey = () => ({ type: CLEAR_PRESSED_KEY });
-export const clearSelectNote = () => ({ type: CLEAR_SELECT_NOTE });
-export const exportTabFormatTxt = () => ({ type: EXPORT_TAB_TXT });
-export const holdPressedKey = (key_code) => ({ type: HOLD_PRESSED_KEY, key_code });
-export const moveSelectedNote = (key_code) => ({ type: MOVE_SELECTED_NOTE, key_code });
-export const removeBlock = (part_id) => ({ type: REMOVE_BLOCK, part_id });
-// eslint-disable-next-line max-params
-export const selectNote = (p, b, c, l) => ({ type: SELECT_NOTE, p, b, c, l });
-export const updateNote = (key_code) => ({ type: UPDATE_NOTE, key_code });
-export const userChangedWritingType = ({ writing }) => ({ type: CHANGED_WRITING_TYPE, writing });
+export const addBlock = (part_id) => ({
+	type: types.ADD_BLOCK,
+	part_id,
+});
+
+export const addPart = () => ({
+	type: types.ADD_PART,
+});
+
+export const clearPressedKey = () => ({
+	type: types.CLEAR_PRESSED_KEY,
+});
+
+export const clearSelectNote = () => ({
+	type: types.CLEAR_SELECT_NOTE,
+});
+
+export const exportTabFormatTxt = () => ({
+	type: types.EXPORT_TAB_TXT,
+});
+
+export const holdPressedKey = (key_code) => ({
+	type: types.HOLD_PRESSED_KEY,
+	key_code,
+});
+
+export const moveSelectedNote = (key_code) => ({
+	type: types.MOVE_SELECTED_NOTE,
+	key_code,
+});
+
+export const removeBlock = (part_id) => ({
+	type: types.REMOVE_BLOCK,
+	part_id,
+});
+
+export const selectNote = (note) => ({
+	type: types.SELECT_NOTE,
+	note,
+});
+
+export const updateNote = (key_code) => ({
+	type: types.UPDATE_NOTE,
+	key_code,
+});
+
+export const userChangedWritingType = (writing) => ({
+	type: types.CHANGED_WRITING_TYPE,
+	writing,
+});
+
+export const addChordToLyric = ({ part_id, line_id }) => ({
+	type: types.ADD_CHORD_TO_LYRIC,
+	part_id,
+	line_id,
+});

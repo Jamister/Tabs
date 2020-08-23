dcfac256473fa24100c32cdb51a782a99e42a4d4
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-08-22 23:27:52 -0300

    [master/web] fix: Add date when creating uuids

--- web/src/modules/shared/utils/createUniqueId/createUniqueId.js
+++ web/src/modules/shared/utils/createUniqueId/createUniqueId.js
@@ -1,7 +1,8 @@
 import { v5 as uuidv5 } from 'uuid';
 
 export const createUniqueId = (hash) => {
-    const uuid = uuidv5(hash, process.env.REACT_APP_NAMESPACE);
+    const hash_with_date = `${hash}-${Date.now()}`;
+    const uuid = uuidv5(hash_with_date, process.env.REACT_APP_NAMESPACE);
     const smaller_id = uuid.split('-')[0];
     return { uuid, smaller_id };
 };

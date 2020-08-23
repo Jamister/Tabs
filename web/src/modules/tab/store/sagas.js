9fae7f7c9897a3f523be65e231707219102f4d88
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-08-23 00:08:16 -0300

    [master/web] fix: Actions to update note only dispatched when note is selected

--- web/src/modules/tab/store/sagas.js
+++ web/src/modules/tab/store/sagas.js
@@ -1,10 +1,18 @@
-import { takeEvery, put } from 'redux-saga/effects';
+import { takeEvery, put, select } from 'redux-saga/effects';
 import * as types from './types';
 
 export function* handleUpdateNote({ key }) {
-    yield put({ type: types.REMOVE_EMPTY_COLUMN, key });
-    yield put({ type: types.UPDATE_NOTE, key });
-    yield put({ type: types.ADD_COLUMN });
+    const selected_note = yield select(state => state.tab.selected_note);
+    const selected_note_id = Object
+        .values(selected_note)
+        .map(value => value)
+        .join('-');
+    const has_note_selected = selected_note_id !== '0-0-0-0';
+    if (has_note_selected) {
+        yield put({ type: types.REMOVE_EMPTY_COLUMN, key });
+        yield put({ type: types.UPDATE_NOTE, key });
+        yield put({ type: types.ADD_COLUMN });
+    }
 }
 
 export default function* tabSaga() {

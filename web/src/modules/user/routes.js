a75603ca767cf8ead70e926031e5ca8121d37653
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-09-04 23:08:23 -0300

    [master/web] fix: Remove unused components

--- web/src/modules/user/routes.js
+++ web/src/modules/user/routes.js
@@ -1,8 +1,6 @@
 import SignIn from './pages/SignIn';
-import Test1 from './pages/Test1';
 
 const user_routes = [
-    { path: '/test1', exact: true, component: Test1 },
     { path: '/sign/in', exact: true, component: SignIn },
 ];
 

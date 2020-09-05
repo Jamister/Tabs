2aa591339a91fa0fd7089b634984719f31b6320e
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-09-05 20:35:24 -0300

    [master/api] feat: Create tab mutation

--- api/src/Tab/mutation.js
+++ api/src/Tab/mutation.js
@@ -1,21 +1,30 @@
-const { extendType } = require('@nexus/schema');
-const { extractToken, decodeJwt } = require('../utils/tokens');
+const { extendType, stringArg } = require('@nexus/schema');
+const { decodeJwt } = require('../utils/tokens');
 
 const UserMutation = extendType({
     type: 'Mutation',
     definition(t) {
-        t.field('createDraft', {
+        t.field('createTab', {
             type: 'Tab',
-            args: {},
-            resolve: async (parent, args, ctx) => {
-                const token = extractToken(ctx);
-                const { user_external_id } = decodeJwt(token);
-                const user = await ctx.prisma.user.findOne({
-                    where: { externalId: user_external_id },
+            args: {
+                tab: stringArg(),
+            },
+            resolve: async (parent, args, context) => {
+                const { user_email } = decodeJwt(context);
+                const user = await context.prisma.user.findOne({
+                    where: { email: user_email },
                 });
-                return ctx.prisma.post.create({
+                if (user) {
+                    return context.prisma.tab.create({
+                        data: {
+                            tab: args.tab,
+                            user: { connect: { id: user.id } },
+                        },
+                    });
+                }
+                return context.prisma.tab.create({
                     data: {
-                        user: { connect: { id: user.id } },
+                        tab: args.tab,
                     },
                 });
             },

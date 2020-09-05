c177533b9360910ebd3711b965489981b2f2f67d
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-09-05 14:49:32 -0300

    [master/web] feat: Create tab button working

--- web/src/modules/tab/components/CreateTabButton/CreateTabButton.js
+++ web/src/modules/tab/components/CreateTabButton/CreateTabButton.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import { useHistory } from 'react-router-dom';
 import { Button } from 'antd';
 
-const CreateTabButton = () => (
-    <Button type="secondary">
-        Criar nova tab
-    </Button>
-);
+const CREATE_TAB = gql`
+    mutation CreateTab($tab: String) {
+        createTab(tab: $tab) {
+            hashId
+            tab
+        }
+    }
+`;
+
+const CreateTabButton = () => {
+    const history = useHistory();
+    const [createTab, { data, loading, error }] = useMutation(CREATE_TAB);
+
+    useEffect(() => {
+        if (data) {
+            const tabId = data.hashId;
+            history.push(`/tab/${tabId}`);
+        }
+    }, [data]);
+
+    useEffect(() => {
+        if (error) {
+            console.log(error); // TODO error
+        }
+    }, [error]);
+
+    function create() {
+        createTab({ variables: { tab: '' } });
+    }
+
+    return (
+        <Button
+            type="secondary"
+            loading={loading}
+            onClick={create}
+        >
+            Criar nova tab
+        </Button>
+    );
+};
 
 export default CreateTabButton;

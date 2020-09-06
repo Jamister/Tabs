754e8f2154d282b948206a55c6ce748bd1750a40
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-09-05 23:41:11 -0300

    [master/api] feat: Token verfication ok

--- api/src/Tab/query.js
+++ api/src/Tab/query.js
@@ -1,13 +1,40 @@
 const { extendType, intArg } = require('@nexus/schema');
+const { AuthenticationError } = require('apollo-server-express');
+const { verifyToken } = require('../utils/tokens');
 
 const TabQuery = extendType({
     type: 'Query',
     definition(t) {
-        t.list.field('feed', {
+        t.list.field('allTabs', {
             type: 'Tab',
-            resolve: (_, args, ctx) => (
-                ctx.prisma.tab.findMany()
+            resolve: (_, args, context) => (
+                context.prisma.tab.findMany()
             ),
+        });
+
+        t.list.field('myTabs', {
+            type: 'Tab',
+            resolve: async (_, args, context) => {
+                const { valid, user_email } = verifyToken(context);
+                if (!valid) {
+                    throw new AuthenticationError('You must be logged in');
+                }
+
+                const user = await context.prisma.user.findOne({
+                    where: { email: user_email },
+                });
+                if (user) {
+                    return context.prisma.tab.findMany({
+                        where: {
+                            user: {
+                                id: user.id,
+                            },
+                        },
+                    });
+                }
+
+                throw new Error('User not found');
+            },
         });
 
         t.field('tab', {
@@ -15,8 +42,8 @@ const TabQuery = extendType({
             args: {
                 id: intArg(),
             },
-            resolve: (_, { id }, ctx) => (
-                ctx.prisma.tab.findOne({
+            resolve: (_, { id }, context) => (
+                context.prisma.tab.findOne({
                     where: { id },
                 })
             ),
--- api/src/utils/tokens.js
+++ api/src/utils/tokens.js
@@ -11,51 +11,51 @@ const base64urlDecode = (str) => (
     Buffer.from(base64urlUnescape(str), 'base64').toString()
 );
 
-const extractToken = (context) => {
-    const authorization = context.req.headers.authorization || null;
-    return authorization !== null
-        ? authorization.replace('Bearer ', '')
-        : '';
-};
+// const extractToken = (context) => {
+//     const authorization = context.req.headers.authorization || null;
+//     return authorization !== null
+//         ? authorization.replace('Bearer ', '')
+//         : '';
+// };
 
-const decodeJwt = (context) => {
-    const token = extractToken(context);
-    const segments = token.split('.');
+// const decodeJwt = (context) => {
+//     const token = extractToken(context);
+//     const segments = token.split('.');
 
-    if (segments.length !== 3) {
-        throw new Error('Not enough or too many segments');
-    }
+//     if (segments.length !== 3) {
+//         throw new Error('Not enough or too many segments');
+//     }
 
-    const [headerSeg, payloadSeg, signature] = segments;
-    const header = JSON.parse(base64urlDecode(headerSeg));
-    const payload = JSON.parse(base64urlDecode(payloadSeg));
+//     const [headerSeg, payloadSeg, signature] = segments;
+//     const header = JSON.parse(base64urlDecode(headerSeg));
+//     const payload = JSON.parse(base64urlDecode(payloadSeg));
 
-    const user_external_id = (payload || {}).sub || '';
-    const user_email = (payload || {}).email || '';
+//     const user_external_id = (payload || {}).sub || '';
+//     const user_email = (payload || {}).email || '';
 
-    // console.log('header', header);
-    // console.log('signature', signature);
-    // console.log('payload', payload);
+//     // console.log('header', header);
+//     // console.log('signature', signature);
+//     // console.log('payload', payload);
 
-    return {
-        header,
-        payload,
-        signature,
-        user_external_id,
-        user_email,
-    };
-};
+//     return {
+//         header,
+//         payload,
+//         signature,
+//         user_external_id,
+//         user_email,
+//     };
+// };
 
-const isExpired = (context) => {
-    const { payload, user_email } = decodeJwt(context);
-    // console.log('user_email', user_email);
-    const now = Date.now().valueOf() / 1000;
-    const expiration = (payload || {}).exp || null;
-    // console.log('expiration', expiration);
-    const is_expired = expiration === null || now > expiration;
-    // console.log('is_expired', is_expired);
-    return is_expired;
-};
+// const isExpired = (context) => {
+//     const { payload, user_email } = decodeJwt(context);
+//     // console.log('user_email', user_email);
+//     const now = Date.now().valueOf() / 1000;
+//     const expiration = (payload || {}).exp || null;
+//     // console.log('expiration', expiration);
+//     const is_expired = expiration === null || now > expiration;
+//     // console.log('is_expired', is_expired);
+//     return is_expired;
+// };
 
 const googleValidation = (token) => {
     const client = new OAuth2Client(process.env.GOOGLE_AUTH_CLIENT_ID);
@@ -77,16 +77,17 @@ const createToken = (email) => jwt.sign(
     { expiresIn: 15552000 }, // 6 months
 );
 
-const verifyToken = (token) => jwt.verify(
-    token,
-    process.env.SECRET,
-);
+const verifyToken = (context) => {
+    const token = context.req.headers.token || '';
+    return jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) return { valid: false, user_email: null };
+        return { valid: true, user_email: decoded.email || '' };
+    });
+};
 
 module.exports = {
-    extractToken,
-    decodeJwt,
-    isExpired,
-    newValidation: () => {},
+    isExpired: () => {},
+    decodeJwt: () => {},
     googleValidation,
     createToken,
     verifyToken,

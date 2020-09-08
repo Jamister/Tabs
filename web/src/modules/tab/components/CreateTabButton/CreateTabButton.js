06de40518bca48416a2cf1512f01e8d92a34c460
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-09-07 23:28:24 -0300

    [master/web] fix: Create tab button working

--- web/src/modules/tab/components/CreateTabButton/CreateTabButton.js
+++ web/src/modules/tab/components/CreateTabButton/CreateTabButton.js
@@ -1,26 +1,42 @@
 import React, { useEffect } from 'react';
-import { gql, useMutation } from '@apollo/client';
+import { useMutation } from '@apollo/client';
 import { useHistory } from 'react-router-dom';
 import { Button } from 'antd';
 
-const CREATE_TAB = gql`
-    mutation CreateTab($tab: String) {
-        createTab(tab: $tab) {
-            hashId
-            tab
-        }
-    }
-`;
+// Api
+import { CREATE_TAB } from 'modules/tab/api';
+
+// Utils
+import { isUserLogged } from 'modules/user/utils/isUserLogged';
 
 const CreateTabButton = () => {
     const history = useHistory();
     const [createTab, { data, loading, error }] = useMutation(CREATE_TAB);
 
     useEffect(() => {
-        if (data) {
-            const tabId = data.hashId;
+        function redirect(tabId) {
             history.push(`/tab/${tabId}`);
         }
+
+        function saveTabIdToAssignInTheFuture(tabId) {
+            const previousTabsToAssign = localStorage.getItem('tabsToAssign') || '';
+            const tabsToAssign = `${previousTabsToAssign},${tabId}`;
+            localStorage.setItem('tabsToAssign', tabsToAssign);
+            redirect();
+        }
+
+        function checkIfUserIsLogged(tabId) {
+            return isUserLogged()
+                ? redirect(tabId)
+                : saveTabIdToAssignInTheFuture(tabId);
+        }
+
+        function checkData() {
+            const tabId = data?.createTab?.hashId;
+            if (tabId) checkIfUserIsLogged(tabId);
+        }
+
+        checkData();
     }, [data]);
 
     useEffect(() => {

990c82f9c53126b15fa2ea0d1dd42ed99a3af530
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-09-27 22:14:45 -0300

    [master/web] feat: Remove part if last block was removed and fix bug finding last block

--- web/src/modules/tab/store/logic/removeBlock/removeBlock.js
+++ web/src/modules/tab/store/logic/removeBlock/removeBlock.js
@@ -3,9 +3,12 @@ import produce from 'immer';
 import { getDateNow } from 'modules/shared/utils/dates';
 
 const removeBlock = produce((draft, action) => {
+    const partId = action?.part_id || '';
     const blocks = draft.blocks || {};
     const blocks_all_ids = blocks.all_ids || [];
     const blocks_by_id = blocks.by_id || {};
+    const blocksInThisPart = blocks_all_ids
+        .filter(b => b.indexOf(`${partId}-`) !== -1);
 
     function finish() {
         return draft;
@@ -16,63 +19,76 @@ const removeBlock = produce((draft, action) => {
         return finish();
     }
 
-    function removeNotesInLastBlock(last_block_id) {
+    function removePartFromById() {
+        const partsById = { ...draft?.parts?.by_id };
+        delete partsById[partId];
+        draft.parts.by_id = partsById;
+        return setLastChange();
+    }
+
+    function removePartFromAllIds() {
+        const partsAllIds = draft?.parts?.all_ids || [];
+        draft.parts.all_ids = partsAllIds.filter(p => p !== partId);
+        return removePartFromById();
+    }
+
+    function removePartIfIsLastBlock() {
+        const isLastBlockInThisPart = blocksInThisPart.length === 1;
+        return isLastBlockInThisPart
+            ? removePartFromAllIds()
+            : setLastChange();
+    }
+
+    function removeNotesInLastBlock(lastBlockFullId) {
         const notes = { ...draft.notes };
         const notes_ids = Object.keys(notes) || [];
         notes_ids.forEach(note_id => {
-            const to_delete = note_id.indexOf(`${last_block_id}-`) === 0;
+            const to_delete = note_id.indexOf(`${lastBlockFullId}-`) === 0;
             if (to_delete) delete notes[note_id];
             return !to_delete;
         });
         draft.notes = notes;
-        return setLastChange();
+        return removePartIfIsLastBlock();
     }
 
-    function removeColumnsInLastBlock(last_block_full_id) {
+    function removeColumnsInLastBlock(lastBlockFullId) {
         const columns = draft.columns || {};
         const new_columns_by_id = { ...columns.by_id || {} };
         const new_columns_all_ids = (columns.all_ids || [])
             .filter(id => {
-                const to_delete = id.indexOf(`${last_block_full_id}-`) === 0;
+                const to_delete = id.indexOf(`${lastBlockFullId}-`) === 0;
                 if (to_delete) delete new_columns_by_id[id];
                 return !to_delete;
             });
         draft.columns.all_ids = new_columns_all_ids;
         draft.columns.by_id = new_columns_by_id;
-        return removeNotesInLastBlock(last_block_full_id);
+        return removeNotesInLastBlock(lastBlockFullId);
     }
 
-    function removeBlockFromById(last_block_full_id) {
+    function removeBlockFromById(lastBlockFullId) {
         const new_blocks_by_id = { ...blocks_by_id };
-        delete new_blocks_by_id[last_block_full_id];
+        delete new_blocks_by_id[lastBlockFullId];
         draft.blocks.by_id = new_blocks_by_id;
-        return removeColumnsInLastBlock(last_block_full_id);
+        return removeColumnsInLastBlock(lastBlockFullId);
     }
 
-    function removeBlockFromAllIds(last_block_full_id) {
-        blocks_all_ids.pop();
-        const new_blocks_all_ids = blocks_all_ids;
-        draft.blocks.all_ids = new_blocks_all_ids;
-        return removeBlockFromById(last_block_full_id);
+    function removeBlockFromAllIds(lastBlockFullId) {
+        const blocksAllIdsUpdated = blocks_all_ids
+            .filter(blockId => blockId !== lastBlockFullId);
+        draft.blocks.all_ids = blocksAllIdsUpdated;
+        return removeBlockFromById(lastBlockFullId);
     }
 
-    function getLastBlockId(part_id, blocks_in_this_part) {
-        const [last_block_full_id] = blocks_in_this_part.slice(-1);
-        return removeBlockFromAllIds(last_block_full_id);
-    }
-
-    function findBlocksOfThisPart(part_id) {
-        const blocks_in_this_part = blocks_all_ids
-            .filter(b => b.indexOf(`${part_id}-`) !== -1);
-        return getLastBlockId(part_id, blocks_in_this_part);
+    function getLastBlockId() {
+        const [lastBlockFullId] = blocksInThisPart.slice(-1);
+        return removeBlockFromAllIds(lastBlockFullId);
     }
 
     function checkEmptyParams() {
-        const part_id = action.part_id || '';
-        const empty_id = part_id === '';
+        const empty_id = partId === '';
         return empty_id
             ? finish()
-            : findBlocksOfThisPart(part_id);
+            : getLastBlockId();
     }
 
     return checkEmptyParams();

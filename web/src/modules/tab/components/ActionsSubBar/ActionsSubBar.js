13a22f8f01db5a7d0fe6ef5dbf6ad08ad3568a3d
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-09-28 11:00:14 -0300

    [master/web] feat: Actions subbar fixed on scroll when editing tab

--- web/src/modules/tab/components/ActionsSubBar/ActionsSubBar.js
+++ web/src/modules/tab/components/ActionsSubBar/ActionsSubBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as s from './ActionsSubBar.style';
 
 // Components
@@ -7,17 +7,37 @@ import SwitchWritingType from '../SwitchWritingType';
 import TuningButton from '../TuningButton';
 import VirtualKeyboardSwitch from '../VirtualKeyboardSwitch';
 
-const ActionsSubBar = () => (
-    <s.SubBarWrapper>
-        <Container>
-            <SwitchWritingType />
-            <s.SubBarDivision />
-            <TuningButton />
-            <s.SubBarDivision />
-            <VirtualKeyboardSwitch />
-            <s.SubBarDivision />
-        </Container>
-    </s.SubBarWrapper>
-);
+const ActionsSubBar = () => {
+    const [fixed, setFixed] = useState(false);
+
+    useEffect(() => {
+        function handleScroll() {
+            const scrollPosition = window.scrollY;
+            const fixedScroll = scrollPosition >= 61;
+            setFixed(fixedScroll);
+        }
+
+        window.addEventListener('scroll', handleScroll, { passive: true });
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    return (
+        <>
+            <s.SubBarBackspace visible={fixed} />
+            <s.SubBarWrapper fixed={fixed}>
+                <Container>
+                    <SwitchWritingType />
+                    <s.SubBarDivision />
+                    <TuningButton />
+                    <s.SubBarDivision />
+                    <VirtualKeyboardSwitch />
+                    <s.SubBarDivision />
+                </Container>
+            </s.SubBarWrapper>
+        </>
+    );
+};
 
 export default ActionsSubBar;
--- web/src/modules/tab/components/ActionsSubBar/ActionsSubBar.style.js
+++ web/src/modules/tab/components/ActionsSubBar/ActionsSubBar.style.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import theme from 'styles/theme';
 
 export const SubBarWrapper = styled.div`
@@ -7,6 +7,23 @@ export const SubBarWrapper = styled.div`
     height: ${theme.subbar_height}px;
     background: #EFF0F2;
     border-bottom: 1px solid #E2E6ED;
+
+    ${props => props.fixed && css`
+        position: fixed;
+        top: 0;
+        z-index: 12;
+    `}
+`;
+
+export const SubBarBackspace = styled.div`
+    display: none;
+    position: relative;
+    width: 100%;
+    height: ${theme.subbar_height}px;
+
+    ${props => props.visible && css`
+        display: block;
+    `}
 `;
 
 export const SubBarDivision = styled.div`

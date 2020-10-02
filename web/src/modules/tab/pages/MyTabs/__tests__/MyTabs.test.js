ea6c14bde5154099ef2ab326c385b04953f3cdfe
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-10-01 22:15:29 -0300

    [master/web] tests: MyTabs basic testing working with Apollo client

--- web/src/modules/tab/pages/MyTabs/__tests__/MyTabs.test.js
+++ web/src/modules/tab/pages/MyTabs/__tests__/MyTabs.test.js
@@ -1,7 +1,7 @@
 import 'tests/matchMedia.mock';
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, wait, waitForElement, act } from '@testing-library/react';
+import { render, screen, waitForElement } from '@testing-library/react';
 import { MockedProvider } from '@apollo/client/testing';
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -22,69 +22,34 @@ const mocks = {
     request: {
         query: MY_TABS,
     },
-    newData: jest.fn(() => ({
+    result: {
         data: {
             myTabs: [
                 {
                     hashId: '4h3j4n',
                     title: 'MyTabTitle',
-                    artist: '',
-                    tuning: '',
+                    artist: 'My Artist 1',
+                    tuning: 'e B G D A D',
+                    instrument: 'guitar',
                     private: false,
                 }
             ],
         },
-    })),
+    },
 };
 
 describe('MyTabs', () => {
     it('should not break', async () => {
-        const { getByTestId } = render(
+        render(
             <MockedProvider mocks={[mocks]} addTypename={false}>
                 <Router>
                     <MyTabs />
                 </Router>
             </MockedProvider>,
         );
-
-        await new Promise(resolve => setTimeout(resolve, 0));
-        screen.getByText(/Loading.../i);
-        // await new Promise(resolve => setTimeout(resolve, 0));
-
-        const addBookMutationMock = mocks.newData;
-        await wait(() => expect(addBookMutationMock).toHaveBeenCalled());
         screen.getByText(/Minhas tablaturas/i);
-
-        // await waitForElement(() => getByTestId('lis'));
-        // screen.getByText(/MyTabTitle/i);
-        // console.log(screen);
-
-        // screen.queryAllByText('Loading...');
-
-        
-        // expect(
-        //     screen.queryAllByText('Loading...')
-        // ).toHaveLength(0);
-        // expect(
-        //     screen.queryAllByText('MyTabTitle1')
-        // ).toHaveLength(1);
+        await waitForElement(() => screen.queryAllByText('MyTabTitle'));
+        await waitForElement(() => screen.getByText(/My Artist 1/i));
+        await waitForElement(() => screen.getByText(/e B G D A D/i));
     });
-
-//   it("should show error in the card number field", async () => {
-//     render(<App />);
-//     const cardNumberInput = screen.getByLabelText("Card number");
-//     fireEvent.change(cardNumberInput, { target: { value: "" } });
-//     fireEvent.blur(cardNumberInput);
-//     expect(
-//       screen.queryAllByText("Please provide a valid credit card number")
-//     ).toHaveLength(1);
-//     fireEvent.change(cardNumberInput, { target: { value: "432565" } });
-//     expect(
-//       screen.queryAllByText("Please provide a valid credit card number")
-//     ).toHaveLength(0);
-//     fireEvent.blur(cardNumberInput);
-//     expect(
-//       screen.queryAllByText("Please provide a valid credit card number")
-//     ).toHaveLength(1);
-//   });
 });

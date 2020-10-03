69445101a420c453d39e0ed860b7d019fb54ec3a
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-10-03 17:36:53 -0300

    [master/web] tests: Test for clearSelectNote use case

--- web/src/modules/tab/store/logic/clearSelectNote/__tests__/clearSelectNote.test.js
+++ web/src/modules/tab/store/logic/clearSelectNote/__tests__/clearSelectNote.test.js
@@ -1,29 +1,20 @@
+import produce from 'immer';
 import clearSelectNote from '../clearSelectNote';
+import tab from '../../../store';
 
-test('clearSelectNote should not crash', () => {
-    const test_empty = clearSelectNote();
-    expect(test_empty).toStrictEqual({
-        selected_note: {
-            p: 0,
-            b: 0,
-            c: 0,
-            l: 0,
-        },
-    });
-});
-
-test('should clear the selected note', () => {
-    const state = {
-        blocks: {},
-    };
-    const result = clearSelectNote(state);
-    expect(result).toStrictEqual({
-        blocks: {},
-        selected_note: {
-            p: 0,
-            b: 0,
-            c: 0,
-            l: 0,
-        },
+describe('clearSelectNote', () => {
+    it('should clear the selected note', () => {
+        const state = produce(tab, draft => {
+            draft.selected_note = {
+                p: '2gfd',
+                b: '0n5h',
+                c: '1fg6',
+                l: '2',
+            };
+        });
+        const result = clearSelectNote(state);
+        expect(result.selected_note).toStrictEqual({
+            p: 0, b: 0, c: 0, l: 0,
+        });
     });
 });

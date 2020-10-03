6b2bdb3148d3076d280feb64b597472e766e6de9
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-10-03 16:18:43 -0300

    [master/web] tests: Add test for changedWritingType use case

---
+++ web/src/modules/tab/store/logic/changedWritingType/__tests__/changedWritingType.test.js
@@ -0,0 +1,20 @@
+import produce from 'immer';
+import changedWritingType from '../changedWritingType';
+import tab from '../../../store';
+
+describe('changedWritingType', () => {
+    it('should change the writing to chords', () => {
+        const action = { writing: 'chords' };
+        const result = changedWritingType(tab, action);
+        expect(result.user_is_writing).toBe('chords');
+    });
+
+    it('should change the writing to notes', () => {
+        const state = produce((draft) => {
+            draft.user_is_writing = 'chords';
+        });
+        const action = { writing: 'notes' };
+        const result = changedWritingType(state, action);
+        expect(result.user_is_writing).toBe('notes');
+    });
+});

70cc1481d6206b655f92450aec0badfc076bdbbb
Beto Figueiredo <herberto_f@yahoo.com.br>
2020-10-03 14:43:57 -0300

    [master/web] feat: Include vibrato (~)

--- web/src/modules/tab/utils/validChars.js
+++ web/src/modules/tab/utils/validChars.js
@@ -1,6 +1,6 @@
 export const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 export const letters = ['s', 'h', 'p', 'b', 'r', 'v', 't', 'x'];
-export const specialChars = ['\\', '/'];
+export const specialChars = ['\\', '/', '~'];
 const validChars = [
     ...numbers,
     ...letters,
